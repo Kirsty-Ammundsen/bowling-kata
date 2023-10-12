38a54ec0e591b730ba89e616ad2bfa409d662190
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-10-12 15:25:04 +1300

    Attempt debugging

--- score.js
+++ score.js
@@ -1,6 +1,6 @@
 export { scoreFrame, gameTotal }
 
-const game = [
+const game1 = [
   [1, 2],
   [6, 4],
   [5, 4],
@@ -13,28 +13,39 @@ const game = [
   [4, 4],
 ]
 
+gameTotal(game1)
+
 function scoreFrame(frame, frame2, frame3) {
   // double strike
   if (frame[0] === 10 && frame2[0] === 10) {
+    console.log('double strike ')
     return frame[0] + frame2[0] + frame3.reduce((a, b) => a + b)
   }
   // single strike
   if (frame[0] === 10) {
+    console.log('single strike ')
+
     return frame[0] + frame2.reduce((a, b) => a + b)
   }
   // spare
   if (frame[0] + frame[1] === 10) {
+    console.log('spare ')
     return frame.reduce((a, b) => a + b) + frame2[0]
   }
+
   // normal sum
-  else return frame.reduce((a, b) => a + b)
+  else console.log('normal ')
+  return frame.reduce((a, b) => a + b)
 }
 
 function gameTotal(game) {
   let totals = 0
   for (let i = 0; i < game.length; i++) {
     totals += scoreFrame(game[i], game[i + 1], game[i + 2])
+    console.log(totals)
   }
+
+  console.log(totals)
 
   return totals
 }
