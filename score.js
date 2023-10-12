b2ebffde0a221d4ad0210c646ddb0c88db5753c9
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-10-12 14:16:17 +1300

    Add simple strike score

--- score.js
+++ score.js
@@ -1,5 +1,10 @@
 export { scoreFrame }
 
-function scoreFrame(frame) {
-  return frame[0] + frame[1]
+function scoreFrame(frame, frame2) {
+  if (frame[0] === 10) {
+    return frame[0] + frame2.reduce((a, b) => a + b)
+  }
+  if (frame[0] + frame[1] === 10) {
+    return frame.reduce((a, b) => a + b) + frame2[0]
+  } else return frame[0] + frame[1]
 }
--- score.test.js
+++ score.test.js
@@ -19,3 +19,19 @@ test('scores a normal frame', () => {
   const actual = score.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
+
+test('scores a spare frame', () => {
+  const frame = [4, 6]
+  const frame2 = [5, 7]
+  const expected = 15
+  const actual = score.scoreFrame(frame, frame2)
+  expect(actual).toBe(expected)
+})
+
+test('scores a single strike frame', () => {
+  const frame = [10, 0]
+  const frame2 = [4, 5]
+  const expected = 19
+  const actual = score.scoreFrame(frame, frame2)
+  expect(actual).toBe(expected)
+})
